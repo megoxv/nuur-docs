7434b41e7c31d8b899e3bcc93352d5cf60331434
Abdelmjid Saber <87904671+megoxv@users.noreply.github.com>
2024-06-23 09:58:14 +0300

    Initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Nuur</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/megoxv/nuur-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/megoxv/nuur-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Nuur Docs',
   },
 }
 
